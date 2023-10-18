2efb500d691c68de2ab11188d919e454eaa2162c
RiccardoSignorini <sindaci-fortini-0i@icloud.com>
2023-10-18 15:01:24 +0200

    create file components + service

---
+++ src/app/components/display/display.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-display',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.scss']
+})
+export class DisplayComponent {
+
+}
---
+++ src/app/components/keyboard/keyboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-keyboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './keyboard.component.html',
+  styleUrls: ['./keyboard.component.scss']
+})
+export class KeyboardComponent {
+
+}
