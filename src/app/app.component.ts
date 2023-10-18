3033bd6821c027855dd43bac2e9c67c14c484bb8
RiccardoSignorini <sindaci-fortini-0i@icloud.com>
2023-10-18 16:10:57 +0200

    add connection app display keyboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DisplayComponent } from './components/display/display.component';
+import { KeyboardComponent } from './components/keyboard/keyboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DisplayComponent, KeyboardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-calculator';
+
 }
