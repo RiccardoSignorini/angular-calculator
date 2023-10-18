fc0bc9d682772d0165981b6a9ce6d6c7eca911da
RiccardoSignorini <sindaci-fortini-0i@icloud.com>
2023-10-18 16:27:32 +0200

    add changes style

--- src/app/components/keyboard/keyboard.component.ts
+++ src/app/components/keyboard/keyboard.component.ts
@@ -15,7 +15,7 @@ export class KeyboardComponent {
   constructor()
   {
     this.numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    this.operators = ['+', '*', '-', '/']
+    this.operators = ['+', '*', '-', '/', '=']
   }
 
 }
