401f3d929cdf9671a1d08564389054c74e29de22
RiccardoSignorini <sindaci-fortini-0i@icloud.com>
2023-10-18 16:11:55 +0200

    add keyboard (number+operators calculator)

--- src/app/components/keyboard/keyboard.component.ts
+++ src/app/components/keyboard/keyboard.component.ts
@@ -9,5 +9,13 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./keyboard.component.scss']
 })
 export class KeyboardComponent {
+  numbers: Number[];
+  operators: string[];
+
+  constructor()
+  {
+    this.numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    this.operators = ['+', '*', '-', '/']
+  }
 
 }
