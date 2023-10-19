d72be86bc7b5cb6ebc941896c2124a646279b4a4
Stefano Tomaiuolo <45236602+SteDeus@users.noreply.github.com>
2023-10-19 09:52:27 +0200

    code cleaning part 1

--- src/app/services/operations.service.ts
+++ src/app/services/operations.service.ts
@@ -6,4 +6,20 @@ import { Injectable } from '@angular/core';
 export class OperationsService {
 
   constructor() { }
+
+  public sum(a: number, b: number): number {
+    return a + b;
+  }
+
+  public substract(a: number, b: number): number {
+    return a - b;
+  }
+
+  public multiply(a: number, b: number): number {
+    return a * b;
+  }
+
+  public divide(a: number, b: number): number {
+    return a / b;
+  }
 }
